974dbc4d3b9b32fd3b0e115ccabcf42db051c9e3
Bak Balazs <bak.balazs95@gmail.com>
2017-04-07 11:52:24 +0200

    Bérlethozzáadása átalakítva és numbertextfiledé alakítva és beálítva hogy óra , nap és hónap megadásánál csak 2 szám legyen megadható

--- kondibazis-ui/src/main/java/hu/unideb/inf/kondibazis/ui/kezelo/BerletLetrehozasFoAblakKezelo.java
+++ kondibazis-ui/src/main/java/hu/unideb/inf/kondibazis/ui/kezelo/BerletLetrehozasFoAblakKezelo.java
@@ -3,11 +3,37 @@ package hu.unideb.inf.kondibazis.ui.kezelo;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import hu.unideb.inf.kondibazis.ui.bevitel.szam.NumberTextField;
+import hu.unideb.inf.kondibazis.ui.bevitel.szam.NumberTextFieldLimited;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class BerletLetrehozasFoAblakKezelo implements Initializable {
+	
+	@FXML
+	private Button letrehozasGomb;
+
+	@FXML
+	private TextField berletnevBevitel;
+
+	@FXML
+	private NumberTextField berletaraBevitel;
+
+	@FXML
+	private NumberTextFieldLimited oraBevitel;
+
+	@FXML
+	private NumberTextFieldLimited napBevitel;
+
+	@FXML
+	private NumberTextFieldLimited honapBevitel;
+
+	@FXML
+	private Text letrehozasUzenet;
 	
 	@FXML
 	public void letrehozas(ActionEvent event) {
--- kondibazis-ui/src/main/java/hu/unideb/inf/kondibazis/ui/kezelo/BerletTipusHozzaadasaKezelo.java
+++ kondibazis-ui/src/main/java/hu/unideb/inf/kondibazis/ui/kezelo/BerletTipusHozzaadasaKezelo.java
@@ -4,22 +4,44 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import hu.unideb.inf.kondibazis.ui.bevitel.szam.NumberTextField;
+import hu.unideb.inf.kondibazis.ui.bevitel.szam.NumberTextFieldLimited;
 import hu.unideb.inf.kondibazis.ui.felulet.FeluletBetoltese;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class BerletTipusHozzaadasaKezelo implements Initializable {
 
 	FeluletBetoltese feluletBetoltese = new FeluletBetoltese();
-	
+
 	@FXML
 	private Button letrehozasGomb;
-	
+
 	@FXML
 	private Button kihagyasGomb;
+
+	@FXML
+	private TextField berletnevBevitel;
+
+	@FXML
+	private NumberTextField berletaraBevitel;
+
+	@FXML
+	private NumberTextFieldLimited oraBevitel;
+
+	@FXML
+	private NumberTextFieldLimited napBevitel;
+
+	@FXML
+	private NumberTextFieldLimited honapBevitel;
+
+	@FXML
+	private Text letrehozasUzenet;
 
 	@FXML
 	public void letrehozas(ActionEvent event) {
@@ -34,7 +56,7 @@ public class BerletTipusHozzaadasaKezelo implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		letrehozasGomb.setVisible(false);
+
 	}
 
 }
